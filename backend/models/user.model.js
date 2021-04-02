48334fca4465d9338770833257932b2f3a6232bd
Tanmay <deshpande@outlook.in>
2021-04-02 11:06:08 -0700

    Exercise Tracker App
    Exercise Tracker App

---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+        minLength:3
+    },
+},{
+    timestamps:true
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercises from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExerciseList} />
+        <Route path="/edit/:id" exact component={EditExercises} />
+        <Route path="/create" exact component={CreateExercise} />
+        <Route path="/user" exact component={CreateUser} />      
+      </div>
+    </Router>
+  );
+}
+
+export default App;
